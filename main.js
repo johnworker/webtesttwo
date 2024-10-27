4810dbc5578a15cfa748babc1eb4d2c1d02921b9
hunte <besscomdark@gmail.com>
2024-10-27 22:06:56 +0800

    調整

--- main.js
+++ main.js
@@ -109,21 +109,3 @@ $(window).scroll(function () {
     else arrow.stop().fadeOut(arrowTimeInt)
 });
 
-
-//  骰子
-var cube = document.getElementById('cube');
-
-var min = 1;
-var max = 24;
-
-cube.onclick = function () {
-    var xRand = getRandom(max, min);
-    var yRand = getRandom(max, min);
-
-    cube.style.webkitTransform = 'rotateX(' + xRand + 'deg) rotateY(' + yRand + 'deg)';
-    cube.style.transform = 'rotateX(' + xRand + 'deg) rotateY(' + yRand + 'deg)';
-}
-
-function getRandom(max, min) {
-    return (Math.floor(Math.random() * (max - min)) + min) * 90;
-}
